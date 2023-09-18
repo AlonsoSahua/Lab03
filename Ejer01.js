860dcc37d033f2c41f80b593408bce3d17a3b654
AlonsoSahua <alonso.sahuanay@tecsup.edu.pe>
2023-09-18 08:00:46 -0500

    Primero

---
+++ Ejer01.js
@@ -0,0 +1,12 @@
+var nombre = "Carlos";
+console.log(nombre);
+
+var numero = 1000;
+console.log(numero);
+
+var objetos = {
+    nombre : "Juan",
+    apellido : "Perez",
+    Telefono : "999322"
+}
+console.log(objetos);
---
+++ Ejer24.js
@@ -0,0 +1,4 @@
+function miFuncion(a, b){
+    console.log("Suma: " + (a + b));
+}
+miFuncion(2,3)
